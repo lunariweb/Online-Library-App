d793c7f6d5016fd7515b481d42043f03f3e5c054
Nimesha <nimeshadls8@gmail.com>
2020-12-08 21:17:46 +0530

    request books

---
+++ book-request.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Request a book</title>
+
+    <link rel="stylesheet" href="style-book-request.css">
+
+  </head>
+
+  <body>
+
+
+    <div class="contact-title">
+
+      <h1>Can't Find The Book?</h1>
+      <h2>Send Us A Request, So We Can The Book</h2>
+
+    </div>
+
+    <div class="contact-form">
+      <form id="contact-form" method="post" action="">
+        <input name="name" type="text" class="form-control" placeholder="Your Name" required> <br>
+        <input name="email "type="email" class="form-control" placeholder="Your Email" required><br>
+
+          <textarea name="message" class="form-control" placeholder="Your Request" rows="8" required></textarea><br>
+
+          <input type="submit" class="form-control-submit" value="SEND REQUEST">
+
+
+      </form>
+
+    </div>
+  </body>
+
+
+
+
+
+
+</html>
