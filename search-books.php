941bd3c48a896df0d29f6d56656a2b6eda58c70c
Nimesha <nimeshadls8@gmail.com>
2020-12-10 19:07:06 +0530

    added the search fucntion

---
+++ search-books.php
@@ -0,0 +1,193 @@
+ <?php
+ include "db_conn.php";
+  ?>
+
+
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" >
+      <link rel="stylesheet" href="search-books-style.css">
+      <meta charset="utf-8">
+      <title>Seach Books</title>
+    </head>
+
+    <!--- --------------------------------------------------------------------------------------------------->
+    <header>
+     <div class="headcontainer">
+
+
+       <div class="logoplaceholder"><i class="fa fa-book" aria-hidden="true"></i>
+
+       </div>
+       <div class="titletext">
+         <h2>Search Books</h2>
+       </div>
+
+       <div class="menu-place1">
+         <div class="menu-content1">
+           <div class="homeicon">
+             <i class="fa fa-home" aria-hidden="true"></i>
+
+           </div>
+           <div class="hometext">
+             <h4><a href="dashboard.php">Home</a></h4>
+           </div>
+
+
+         </div>
+
+
+       </div>
+       <div class="menu-place2">
+         <div class="menu-content2">
+           <div class="icon2">
+             <i class="fa fa-user" aria-hidden="true"></i>
+
+           </div>
+           <div class="text2">
+             <h4><a href="#">Browse</a></h4>
+           </div>
+
+
+         </div>
+
+       </div>
+       <div class="menu-place3">
+         <div class="menu-content3">
+           <div class="icon3">
+             <i class="fa fa-paper-plane" aria-hidden="true"></i>
+
+           </div>
+           <div class="text3">
+             <h4><a href="book-request.php">Request</a></h4>
+           </div>
+
+
+         </div>
+
+       </div>
+       <div class="menu-place4">
+         <div class="menu-content4">
+           <div class="icon4">
+             <i class="fa fa-phone" aria-hidden="true"></i>
+
+           </div>
+           <div class="text4">
+             <h4><a href="contact-us.php">Contact</a></h4>
+           </div>
+
+
+         </div>
+
+       </div>
+       <div class="menu-place5">
+         <div class="menu-content5">
+           <div class="icon5">
+             <i class="fa fa-cog" aria-hidden="true"></i>
+
+           </div>
+           <div class="text5">
+             <h4><a href="user-settings.php">Settings</a></h4>
+           </div>
+
+
+         </div>
+
+       </div>
+       <div class="menu-place6" href="logout.php">
+
+         <div class="menu-content6">
+           <div class="icon6">
+             <i class="fa fa-sign-out" aria-hidden="true"></i>
+
+           </div>
+           <div class="text6">
+             <h4>
+               <?php
+
+                   session_start();
+
+                   if (isset($_SESSION['id']) && isset($_SESSION['user_name'])){
+
+
+               ?>
+               <a href="logout.php">Logout</a>
+
+               <?php
+                   }else{
+                       header("Location: index.php");
+                   exit();
+                   }
+
+               ?>
+
+
+             </h4>
+           </div>
+
+
+         </div>
+
+       </div>
+
+
+
+      </div>
+     </div>
+
+     </div>
+    </header>
+
+    <!--- ---------------------------------------------------------------------------------------------------------------------------------->
+
+    <body>
+
+      <div class="title">
+        <h1>Search The Library</h1>
+
+      </div>
+
+
+      <div class="form-holder">
+        <form action="search.php" method="post">
+
+          <input type="text" name="search" placeholder="Search">
+          <button type="submit" name="submit-search">Search</button>
+
+        </form>
+
+      </div>
+
+
+
+
+      <div class="article-cotainer">
+
+        <?php
+          $sql = "SELECT * FROM books";
+          $result = mysqli_query($conn, $sql);
+          $queryResults = mysqli_num_rows($result);
+
+          if ($queryResults > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+              echo "<div class='article-box'>
+                  <h3>Book Name: ".$row['book_name']."</h3>
+                  <h4>Type: ".$row['book_type']."</h4>
+                  <p>Link: ".$row['book_link']."</p>
+
+
+              </div>";
+            }
+          }
+
+         ?>
+
+      </div>
+
+
+
+
+
+    </body>
+  </html>
--- search.php
+++ search.php
@@ -1,51 +1,52 @@
-<DOCTYPE html>
-<html>
-<body>
-<link rel="stylesheet" type="text/css" href="style.css">
 <?php
-    include "db_conn.php";
-    
-    if (isset($_POST["submit"])) {
-        $str = $POST["bookname"];
-        $sth = $con->prepare("SELECT * FROM 'booknames' WHERE bookname = '$str'");
-        
-        $sth->setFetchMode(PDO:: FETCH_OBJ);
-        $sth->execute();
-        
-        
-        if ($row = $sth->fetch()) {
-            
-            ?>
-    
-            <br><br><br>
-            <table>
-    
-                <tr>
-                    <th>bookname</th>
-                </tr>
-                
-                <tr>
-                    <td> <?php echo $row->bookname; ?></td>
-                
-                </tr>
-            </table>
-            
-            
-            
-    <?php 
-            
-        }
-    
-    
-            else{
-                echo "Book is not available"; 
+  include "db_conn.php";
+ ?>
+
+ <!DOCTYPE html>
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>Search Results</title>
+   </head>
+   <body>
+
+     <h1>Search Page</h1>
+
+     <div class="article-container">
+       <?php
+          if (isset($_POST['submit-search'])) {
+            $search = mysqli_real_escape_string($conn, $_POST['search']);
+            $sql = "SELECT * FROM books WHERE book_name LIKE '%$search%' OR book_type LIKE '%$search%' OR book_link LIKE '%$search%'";
+
+            $result = mysqli_query($conn, $sql);
+
+            $queryResult = mysqli_num_rows($result);
+
+            echo "<div class='result-count'><h2> There are ".$queryResult." results! <h2> </div>";
+
+            if ($queryResult > 0) {
+              while ($row = mysqli_fetch_assoc($result)) {
+                echo "<div class='article-box'>
+                    <h3>".$row['book_name']."</h3>
+                    <h3>".$row['book_type']."</h3>
+                    <p>".$row['book_link']."</p>
+
+
+                </div>";
+              }
+            }else{
+
+              echo "<h3> Please check the keyword and try again</h3>";
+
             }
-        
-    }
-    
-?>
-    </body>
-    </html>
-    
-    
-</DOCTYPE>
+
+
+          }
+
+        ?>
+
+     </div>
+
+   </body>
+ </html>
